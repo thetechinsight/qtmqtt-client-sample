37ae9acb43e76209278c4efdaac58118fdd49291
Ankur <ankursahu@live.com>
2018-10-07 20:36:13 +0530

    Authentication support added

--- MQTT_Publisher/mainwindow.cpp
+++ MQTT_Publisher/mainwindow.cpp
@@ -9,29 +9,33 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     _pub_client = new QMqttClient(this);
-    _pub_client->setHostname(ui->hostAddressLineEdit->text());
-    _pub_client->setPort(ui->portSpinBox->value());
+
     ui->publishButton->setEnabled(false);
     ui->pingButton->setEnabled(false);
     connect(ui->portSpinBox,QOverload<int>::of(&QSpinBox::valueChanged),_pub_client,&QMqttClient::setPort);
 
     QObject::connect(_pub_client, &QMqttClient::pingResponseReceived, this,&MainWindow::updateLog);
 
-    QObject::connect(_pub_client, &QMqttClient::stateChanged,[=](){
-        const QString content = QDateTime::currentDateTime().toString()
-                + QLatin1String(": State Change")
-                + QString::number(_pub_client->state())
-                + QLatin1Char('\n');
-        ui->log_TextBrowser->insertPlainText("pub state changed :"+content);
+    QObject::connect(_pub_client, &QMqttClient::stateChanged,[=](QMqttClient::ClientState state){
 
-        if(_pub_client->state() == QMqttClient::Disconnected){
+        if(_pub_client->state() != QMqttClient::Connected){
             ui->connectButton->setText("Connect");
             ui->pingButton->setEnabled(false);
             ui->publishButton->setEnabled(false);
+            if(state == QMqttClient::Connecting){
+                ui->log_TextBrowser->insertPlainText(QDateTime::currentDateTime().toString()+": Connecting with Mqtt broker.\n");
+            }else{
+                ui->log_TextBrowser->insertPlainText(QDateTime::currentDateTime().toString()+": Disconnected with Mqtt broker.\n");
+            }
         }else{
+            if(state == QMqttClient::Connected){
+                ui->log_TextBrowser->insertPlainText(QDateTime::currentDateTime().toString()+": Connected with Mqtt broker.\n");
+            }
+
             ui->connectButton->setText("Disconnect");
             ui->pingButton->setEnabled(true);
             ui->publishButton->setEnabled(true);
+            ui->log_TextBrowser->insertPlainText(QDateTime::currentDateTime().toString()+": Now you are able to publish message.\n");
         }
     });
 
@@ -62,21 +66,34 @@ void MainWindow::on_publishButton_clicked()
         return;
     }
     if(_pub_client->state()== QMqttClient::Connected){
-        qint32 bytes= _pub_client->publish(QMqttTopicName(ui->topic_Lineedit->text()),QByteArray(ui->messageLineEdit->text().toUtf8()));
-        ui->log_TextBrowser->insertPlainText(QString("%1 bytes published\n").arg(bytes));
+        _pub_client->publish(QMqttTopicName(ui->topic_Lineedit->text()),QByteArray(ui->messageLineEdit->text().toUtf8()));
+        ui->log_TextBrowser->insertPlainText(QDateTime::currentDateTime().toString()+" "+QString("message '%1' published on the topic '%2'\n").arg(ui->messageLineEdit->text()).arg(ui->topic_Lineedit->text()));
     }
 }
 
 void MainWindow::on_pingButton_clicked()
 {
     _pub_client->requestPing();
+    ui->log_TextBrowser->insertPlainText(QDateTime::currentDateTime().toString()+": Pign Requested.");
 }
 
 void MainWindow::on_connectButton_clicked()
 {
     if(_pub_client->state() == QMqttClient::Disconnected){
+
+        QString host= ui->hostAddressLineEdit->text();
+
+        if(host.isEmpty()){
+            ui->log_TextBrowser->insertPlainText(QDateTime::currentDateTime().toString()+": Please provide a valid host address\n");
+            return;
+        }
+        _pub_client->setHostname(host);
+        _pub_client->setPort(ui->portSpinBox->value());
+        _pub_client->setUsername(ui->usernameEdit->text());
+        _pub_client->setPassword(ui->passwordEdit->text());
         _pub_client->connectToHost();
     }else{
+        _pub_client->unsubscribe(QMqttTopicFilter(ui->topic_Lineedit->text()));
         _pub_client->disconnectFromHost();
     }
 
--- MQTT_SUBSCRIBER/mainwindow.cpp
+++ MQTT_SUBSCRIBER/mainwindow.cpp
@@ -13,23 +13,30 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->subcribeButton->setEnabled(false);
     connect(ui->portSpinBox,QOverload<int>::of(&QSpinBox::valueChanged),_sub_client,&QMqttClient::setPort);
     connect(_sub_client,&QMqttClient::stateChanged,[=](QMqttClient::ClientState state){
-        const QString content = QDateTime::currentDateTime().toString()
-                + QLatin1String(": State Change")
-                + QString::number(_sub_client->state())
-                + QLatin1Char('\n');
-        ui->log_TextBrowser->insertPlainText("pub state changed :"+content);
-
-        if(state == QMqttClient::Connected){
-            ui->pingButton->setEnabled(true);
-            ui->subcribeButton->setEnabled(true);
-        }else{
+        if(state != QMqttClient::Connected){
             ui->pingButton->setEnabled(false);
             ui->subcribeButton->setEnabled(false);
+            ui->connectButton->setText("Connect");
+            if(state == QMqttClient::Connecting){
+                ui->log_TextBrowser->insertPlainText(QDateTime::currentDateTime().toString()+": Connecting with Mqtt broker.\n");
+            }else{
+                ui->log_TextBrowser->insertPlainText(QDateTime::currentDateTime().toString()+": Disconnected with Mqtt broker.\n");
+            }
+        }else{
+            if(state == QMqttClient::Connected){
+                ui->log_TextBrowser->insertPlainText(QDateTime::currentDateTime().toString()+": Connected with Mqtt broker.\n");
+            }
+
+            ui->pingButton->setEnabled(true);
+            ui->subcribeButton->setEnabled(true);
+            ui->connectButton->setText("Disconnect");
+
+            ui->log_TextBrowser->insertPlainText(QDateTime::currentDateTime().toString()+": Now you are able to subscribe topic.\n");
         }
 
     });
     connect(_sub_client,&QMqttClient::errorChanged,this,[=](QMqttClient::ClientError error){
-        ui->log_TextBrowser->insertPlainText("Error occured: "+QString::number(error));
+        ui->log_TextBrowser->insertPlainText(QDateTime::currentDateTime().toString()+": Error occured: "+QString::number(error)+"\n");
     });
     connect(_sub_client,&QMqttClient::pingResponseReceived,[=](){
         const QString content = QDateTime::currentDateTime().toString()
@@ -60,13 +67,17 @@ void MainWindow::on_connectButton_clicked()
         QString host= ui->hostAddressLineEdit->text();
 
         if(host.isEmpty()){
-            ui->log_TextBrowser->insertPlainText("Please provide a valid host address\n");
+            ui->log_TextBrowser->insertPlainText(QDateTime::currentDateTime().toString()+": Please provide a valid host address\n");
             return;
         }
         _sub_client->setHostname(host);
         _sub_client->setPort(ui->portSpinBox->value());
+        _sub_client->setUsername(ui->usernameEdit->text());
+        _sub_client->setPassword(ui->passwordEdit->text());
         _sub_client->connectToHost();
+
     }else{
+        _sub_client->unsubscribe(QMqttTopicFilter(ui->topic_Lineedit->text()));
         _sub_client->disconnectFromHost();
     }
 }
@@ -74,13 +85,15 @@ void MainWindow::on_connectButton_clicked()
 void MainWindow::on_pingButton_clicked()
 {
     _sub_client->requestPing();
+    ui->log_TextBrowser->insertPlainText(QDateTime::currentDateTime().toString()+": Pign Requested.");
 }
 
 void MainWindow::on_subcribeButton_clicked()
 {
     if(ui->topic_Lineedit->text().isEmpty()){
-        ui->log_TextBrowser->insertPlainText("Please provide a valid topic for subscribe.\n");
+        ui->log_TextBrowser->insertPlainText(QDateTime::currentDateTime().toString()+": Please provide a valid topic for subscribe.\n");
         return;
     }
     _sub_client->subscribe(QMqttTopicFilter(ui->topic_Lineedit->text()));
+    ui->log_TextBrowser->insertPlainText(QDateTime::currentDateTime().toString()+": "+QString("Subscribe to %1.").arg(ui->topic_Lineedit->text()));
 }
